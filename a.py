21b3ae4d3aaf9c4b817c1d44dced2706b9dd83e6
praveen <68511537+7032346995praveen@users.noreply.github.com>
2022-06-20 10:35:57 +0530

    testing

---
+++ a.py
@@ -0,0 +1,43 @@
+x=5 # x is of type int
+x="john"  # x is now of type str
+print(x)
+#CASTING
+x=str(3)  # x will be '3'
+y=int(3)  #y will be 3
+z=float(3) #z will be 3.0
+print(x)
+print(y)
+print(z)
+#   TYPE
+a=5
+b="john"
+print(type(x))
+print(type(y))
+#CASE-SENSITIVE
+a=4
+A="sally"
+print(a)
+print(A)
+#assign multiple values
+x,y,z ="orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+#one value to multiple variables
+x=y=z= "orange"
+print(x)
+print(y)
+print(z)
+#unpack a collection
+fruits =["apple","banana", "cherry"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+# OUTPUT VARIABLES
+a="awesome"
+print("python is "+a)
+b=" Python is "
+c =" awesome"
+d =b+c
+print(d)
